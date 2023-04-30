2567718cacab6592ded1d886af31341733151b9d
Finalcreature <finalnadav@gmail.com>
2023-04-30 14:24:27 +0300

    chore: added a function to get store by name && a function to get store items

--- REST_API/app.py
+++ REST_API/app.py
@@ -41,3 +41,19 @@ def create_item(name):
             store["items"].append(new_item)
             return new_item, 201
     return {"message": "store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_items_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "store not found"}, 404
