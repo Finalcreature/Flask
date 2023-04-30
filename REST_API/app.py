32122c09e81a7d38f5b14a27f8cc2e425ca232e4
Finalcreature <finalnadav@gmail.com>
2023-04-30 16:07:47 +0300

    add: new functions

--- REST_API/app.py
+++ REST_API/app.py
@@ -67,3 +67,36 @@ def get_item(item_id):
         return items[item_id]
     except KeyError:
         abort(404, message="item not found")
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "item deleted"}
+    except:
+        abort(404, message="item not found")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    item = items[item_id]
+    if "name" not in item_data or "price" not in item_data:
+        abort(400, message="name and price are required fields")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item
+    except KeyError:
+        abort(404, message="item not found")
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "store deleted"}
+    except:
+        abort(404, message="store not found")
