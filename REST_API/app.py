b74a679f8a85a12f46e50fdca67105a47e640b9a
Finalcreature <finalnadav@gmail.com>
2023-04-30 18:04:15 +0300

    refactor: add swagger & change app.py accordingly

--- REST_API/app.py
+++ REST_API/app.py
@@ -1,102 +1,21 @@
-import uuid
-from flask import Flask, request
-from flask_smorest import abort
-from db import stores, items
+from flask import Flask
+from flask_smorest import Api
 
+from resources.item import blp as itemBlueprint
+from resources.store import blp as storeBlueprint
 
 app = Flask(__name__)
 
-
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
-
-
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    if ("name" not in store_data):
-        abort(400, message="name is required field")
-
-    for store in stores.values():
-        if store_data["name"] == store["name"]:
-            abort(400, message="store already exists")
-
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-    return store, 201
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "1.0.0"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
+api = Api(app)
 
-    if ("price" not in item_data or "store_id" not in item_data or "name" not in item_data):
-        abort(400, message="price, store_id, name and store_id are required fields")
-
-    for item in items.values():
-        if item["name"] == item_data["name"]:
-            abort(400, message="item already exists")
-
-    if item_data["store_id"] not in stores:
-        abort(404, message="store not found")
-
-    item_id = uuid.uuid4().hex
-    item = {**item_data,  "id": item_id}
-    items[item_id] = item
-    return item, 201
-
-
-@ app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
-@ app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="store not found")
-
-
-@ app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message="item not found")
-
-
-@app.delete("/item/<string:item_id>")
-def delete_item(item_id):
-    try:
-        del items[item_id]
-        return {"message": "item deleted"}
-    except:
-        abort(404, message="item not found")
-
-
-@app.put("/item/<string:item_id>")
-def update_item(item_id):
-    item_data = request.get_json()
-    item = items[item_id]
-    if "name" not in item_data or "price" not in item_data:
-        abort(400, message="name and price are required fields")
-
-    try:
-        item = items[item_id]
-        item |= item_data
-        return item
-    except KeyError:
-        abort(404, message="item not found")
-
-
-@app.delete("/store/<string:store_id>")
-def delete_store(store_id):
-    try:
-        del stores[store_id]
-        return {"message": "store deleted"}
-    except:
-        abort(404, message="store not found")
+api.register_blueprint(itemBlueprint)
+api.register_blueprint(storeBlueprint)
